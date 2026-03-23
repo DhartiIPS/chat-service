1563c627bdc539d4d3e5e53dd7e5994b03a538ab
DhartiIPS <dhartis.itpath@gmail.com>
2026-03-23 16:08:34 +0530

    added the user url

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,11 +4,12 @@ import { Chat } from '../chat/entity/chat.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  url: process.env.USER_URL,
   entities: [Chat],
   migrations: ['src/database/migrations/*{.ts,.js}'],
   synchronize: false,
